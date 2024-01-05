44bad57b43b709789bd772b8a655c0300f76ea74
邹章鹏 <zou.zhangpeng@99cloud.net>
2024-01-05 14:34:13 +0800

    处理404异常

---
+++ blog-backend/src/main/java/com/blog/common/exception/CustomErrorController.java
@@ -0,0 +1,29 @@
+package com.blog.common.exception;
+
+/**
+ * @author zouzhangpeng
+ * @desc
+ */
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.common.domain.Result;
+
+/**
+ * @author zouzhangpeng
+ * @desc 处理404异常
+ */
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Result<Void> handleNotFound() {
+        return Result.fail("资源不存在或已被删除");
+    }
+
+}
