6ef6c748fb149e09ccb03bf414e52a1e7ec673ad
zouzhangpeng <zouzhangpeng@163.com>
2024-01-26 23:24:06 +0800

    菜单组件

--- blog-front/src/components/Menu/index.vue
+++ blog-front/src/components/Menu/index.vue
@@ -1,45 +1,54 @@
 <template>
   <a-menu
       :style="{ width: '100%', height: '100%' }"
-      :default-open-keys="['0']"
-      :default-selected-keys="['0_1']"
       show-collapse-button
   >
-    <a-menu-item key="0_0_0" data-obj="1">
-      <template #icon><svg-icon name="snow"/></template>
-      Menu 1
-    </a-menu-item>
-    <a-sub-menu key="0">
-      <template #icon><svg-icon name="snow"/></template>
-      <template #title>Navigation 1</template>
-      <a-menu-item key="0_0">Menu 1</a-menu-item>
-      <a-menu-item key="0_1">Menu 2</a-menu-item>
-      <a-menu-item key="0_2" disabled>Menu 3</a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="1">
-      <template #icon><svg-icon name="snow"/></template>
-      <template #title>Navigation 2</template>
-      <a-menu-item key="1_0">Menu 1</a-menu-item>
-      <a-menu-item key="1_1">Menu 2</a-menu-item>
-      <a-menu-item key="1_2">Menu 3</a-menu-item>
-    </a-sub-menu>
-    <a-sub-menu key="2">
-      <template #icon><svg-icon name="snow"/></template>
-      <template #title>Navigation 3</template>
-      <a-menu-item-group title="Menu Group 1">
-        <a-menu-item key="2_0">Menu 1</a-menu-item>
-        <a-menu-item key="2_1">Menu 2</a-menu-item>
-      </a-menu-item-group>
-      <a-menu-item-group title="Menu Group 2">
-        <a-menu-item key="2_2">Menu 3</a-menu-item>
-        <a-menu-item key="2_3">Menu 4</a-menu-item>
-      </a-menu-item-group>
-    </a-sub-menu>
+    <menu-item :data="menuData"/>
   </a-menu>
 </template>
 
 <script setup lang="ts">
+import MenuItem from '@/components/MenuItem/index.vue'
+import {reactive} from 'vue'
+import type {Menu} from './type'
 
+const menuData = reactive<Array<Menu>>([{
+  code: "home",
+  name: "主页",
+  url: "/home",
+  icon: "home"
+}, {
+  code: "blog-manage",
+  name: "博客管理",
+  url: "",
+  icon: "blog",
+  children: [
+    {
+      code: "category-manage",
+      name: "分类管理",
+      url: "/category-manage",
+      icon: "",
+      children:[{
+        code: "category-manage1",
+        name: "分类管理1",
+        url: "/category-manage",
+        icon: ""
+      }]
+    },
+    {
+      code: "post-manage",
+      name: "文章管理",
+      url: "/post-manage",
+      icon: ""
+    },
+    {
+      code: "tag-manage",
+      name: "标签管理",
+      url: "/tag-manage",
+      icon: ""
+    }
+  ]
+}]);
 </script>
 
 <style scoped>
---
+++ blog-front/src/components/MenuItem/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <template v-for="item in data" :key="item.code">
+    <a-menu-item :key="item.code" v-if="!item.children">
+      <template #icon v-if="item.icon">
+        <svg-icon :name="item.icon"/>
+      </template>
+      {{ item.name }}
+    </a-menu-item>
+    <a-sub-menu :key="item.code" v-if="item.children">
+      <template #icon v-if="item.icon">
+        <svg-icon :name="item.icon"/>
+      </template>
+      <template #title> {{ item.name }}</template>
+      <menu-item :data="item.children"/>
+    </a-sub-menu>
+  </template>
+</template>
+
+<script setup lang="ts">
+import MenuItem from '@/components/MenuItem/index.vue'
+import type {Menu} from "@/components/Menu/type";
+
+defineProps<{
+  data: Array<Menu>
+}>();
+</script>
+
+<style scoped>
+
+</style>
