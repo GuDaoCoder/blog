5160cfb343f17b90c51a1a56a35d33ceb736df4f
zouzhangpeng <zouzhangpeng@163.com>
2024-02-05 22:24:46 +0800

    路由切换进度条

--- blog-front/src/router/index.ts
+++ blog-front/src/router/index.ts
@@ -1,7 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import {adminRoutes} from './routes'
 import {getToken} from "@/utils/auth";
-
+import {startProgress, closeProgress} from '@/plugin/nprogress'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,11 +14,16 @@ const router = createRouter({
  * 导航守卫
  */
 router.beforeEach((to, from, next) => {
+    startProgress()
     if (to.name !== "login" && !getToken()) {
         next({name: 'login'})
     } else {
         next()
     }
+})
+
+router.afterEach(() => {
+    closeProgress()
 })
 
 export default router;
