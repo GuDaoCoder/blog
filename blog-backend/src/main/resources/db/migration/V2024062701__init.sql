6ed87f60abc73a76541ae81f1d926857daeb2f7c
zouzhangpeng <zouzhangpeng@163.com>
2024-06-27 11:09:14 +0800

    集成flyway

--- blog-backend/src/main/resources/db/migration/V2024062701__init.sql
+++ blog-backend/src/main/resources/db/migration/V2024062701__init.sql
@@ -1,87 +1,4 @@
--- auto-generated definition
-create table t_category
-(
-    category_id        bigint auto_increment comment '分类Id'
-        primary key,
-    category_name      varchar(256) not null comment '分类名称',
-    parent_category_id bigint       not null comment '上级分类Id',
-    full_id            varchar(512) not null comment '全路径Id',
-    order_no           int          not null comment '顺序',
-    level              int          not null comment '级别',
-    create_by          bigint       not null comment '创建用户Id',
-    create_time        datetime     not null comment '创建时间',
-    update_by          bigint       not null comment '更新用户Id',
-    update_time        datetime     not null comment '更新时间'
-)
-    comment '文章分类表';
-
--- auto-generated definition
-create table t_post
-(
-    post_id               bigint auto_increment comment '文章Id'
-        primary key,
-    title                 varchar(256) not null comment '标题',
-    summary               text         null comment '摘要',
-    cover_picture_url     varchar(512) not null comment '封面图片链接',
-    status                varchar(64)  not null comment '文章状态',
-    source                varchar(64)  not null comment '文章来源',
-    category_id           bigint       not null comment '所属分类Id',
-    top                   tinyint(1)   not null comment '是否置顶',
-    enable_comment        tinyint(1)   not null comment '是否开启评论',
-    publish_time          datetime     null comment '发布时间',
-    remove_time           datetime     null comment '下架时间',
-    file_last_update_time datetime     not null comment '文章对应文件最后更新时间',
-    create_by             bigint       not null comment '创建用户Id',
-    create_time           datetime     not null comment '创建时间',
-    update_by             bigint       not null comment '更新用户Id',
-    update_time           datetime     not null comment '更新时间'
-)
-    comment '文章信息表';
-
--- auto-generated definition
-create table t_post_content
-(
-    post_content_id int auto_increment comment '文章内容Id'
-        primary key,
-    post_id         bigint   not null comment '文章Id',
-    content         text     null comment '文章内容',
-    create_by       bigint   not null comment '创建用户Id',
-    create_time     datetime not null comment '创建时间',
-    update_by       bigint   not null comment '更新用户Id',
-    update_time     datetime not null comment '更新时间'
-)
-    comment '文章内容表';
-
--- auto-generated definition
-create table t_post_tag_rela
-(
-    post_tag_rela_id bigint auto_increment comment '文章标签关系主键'
-        primary key,
-    post_id          bigint   not null comment '文章Id',
-    tag_id           bigint   not null comment '标签Id',
-    create_by        bigint   not null comment '创建用户Id',
-    create_time      datetime not null comment '创建时间',
-    update_by        bigint   not null comment '更新用户Id',
-    update_time      datetime not null comment '更新时间'
-)
-    comment '文章标签关系表';
-
--- auto-generated definition
-create table t_tag
-(
-    tag_id      bigint auto_increment comment '标签Id'
-        primary key,
-    tag_name    varchar(128) not null comment '标签名称',
-    color       varchar(20)  not null comment '颜色',
-    create_by   bigint       not null comment '创建用户Id',
-    create_time datetime     not null comment '创建时间',
-    update_by   bigint       not null comment '更新用户Id',
-    update_time datetime     not null comment '更新时间'
-)
-    comment '标签表';
-
--- auto-generated definition
-create table t_user
+create table t_admin_user
 (
     user_id         bigint auto_increment comment '用户Id'
         primary key,
@@ -98,23 +15,114 @@ create table t_user
     create_time     datetime     not null comment '创建时间',
     update_by       bigint       not null comment '更新用户Id',
     update_time     datetime     not null comment '更新时间'
-)
-    comment '用户信息表';
+)comment '管理员信息表';
 
--- auto-generated definition
-create table t_blog_git_repository
+create table t_category
 (
-    repository_id bigint auto_increment comment '主键'
+    category_id        bigint auto_increment comment '分类Id'
         primary key,
-    url           varchar(256) not null comment '仓库地址',
-    local_path    varchar(256) not null comment '本地下载路径',
-    username      varchar(256) null comment '用户名',
-    password      varchar(512) null comment '密码',
-    branch        varchar(64)  null comment '分支',
-    create_by     bigint       not null comment '创建人Id',
-    create_time   datetime     not null comment '创建时间',
-    update_by     bigint       not null comment '更新人Id',
-    update_time   datetime     not null comment '更新时间'
-)
-    comment '博客git仓库地址';
+    category_name      varchar(256) not null comment '分类名称',
+    parent_category_id bigint       not null comment '上级分类Id',
+    full_id            varchar(512) not null comment '全路径Id',
+    order_no           int          not null comment '顺序',
+    level              int          not null comment '级别',
+    create_by          bigint       not null comment '创建用户Id',
+    create_time        datetime     not null comment '创建时间',
+    update_by          bigint       not null comment '更新用户Id',
+    update_time        datetime     not null comment '更新时间'
+)comment '文章分类表';
+
+create table t_git_repository
+(
+    git_repository_id bigint auto_increment comment '主键'
+        primary key,
+    url               varchar(256) not null comment '仓库地址',
+    local_path        varchar(256) not null comment '本地下载路径',
+    username          varchar(256) null comment '用户名',
+    password          varchar(512) null comment '密码',
+    branch            varchar(64)  null comment '分支',
+    create_by         bigint       not null comment '创建人Id',
+    create_time       datetime     not null comment '创建时间',
+    update_by         bigint       not null comment '更新人Id',
+    update_time       datetime     not null comment '更新时间'
+)comment '博客git仓库地址';
+
+create table t_post
+(
+    post_id               bigint auto_increment comment '文章Id'
+        primary key,
+    title                 varchar(256) not null comment '标题',
+    summary               text         null comment '摘要',
+    cover_picture_url     varchar(512) not null comment '封面图片链接',
+    status                varchar(20)  not null comment '文章状态',
+    source                varchar(20)  not null comment '文章来源',
+    category_id           bigint       not null comment '所属分类Id',
+    top                   tinyint(1)   not null comment '是否置顶',
+    enable_comment        tinyint(1)   not null comment '是否开启评论',
+    publish_time          datetime     null comment '发布时间',
+    remove_time           datetime     null comment '下架时间',
+    file_last_update_time datetime     not null comment '文章对应文件最后更新时间',
+    create_by             bigint       not null comment '创建用户Id',
+    create_time           datetime     not null comment '创建时间',
+    update_by             bigint       not null comment '更新用户Id',
+    update_time           datetime     not null comment '更新时间'
+)comment '文章信息表';
+
+create table t_post_content
+(
+    post_content_id int auto_increment comment '文章内容Id'
+        primary key,
+    post_id         bigint   not null comment '文章Id',
+    content         text     null comment '文章内容',
+    create_by       bigint   not null comment '创建用户Id',
+    create_time     datetime not null comment '创建时间',
+    update_by       bigint   not null comment '更新用户Id',
+    update_time     datetime not null comment '更新时间'
+)comment '文章内容表';
+
+create table t_post_tag_rela
+(
+    post_tag_rela_id bigint auto_increment comment '文章标签关系主键'
+        primary key,
+    post_id          bigint   not null comment '文章Id',
+    tag_id           bigint   not null comment '标签Id',
+    create_by        bigint   not null comment '创建用户Id',
+    create_time      datetime not null comment '创建时间',
+    update_by        bigint   not null comment '更新用户Id',
+    update_time      datetime not null comment '更新时间'
+)comment '文章标签关系表';
+
+create table t_tag
+(
+    tag_id      bigint auto_increment comment '标签Id'
+        primary key,
+    tag_name    varchar(128) not null comment '标签名称',
+    color       varchar(20)  not null comment '颜色',
+    create_by   bigint       not null comment '创建用户Id',
+    create_time datetime     not null comment '创建时间',
+    update_by   bigint       not null comment '更新用户Id',
+    update_time datetime     not null comment '更新时间'
+)comment '标签表';
+
+create table t_task
+(
+    task_id         bigint auto_increment comment '主键'
+        primary key,
+    task_name       varchar(64)   not null comment '任务名称',
+    status          varchar(20)   not null comment '任务状态',
+    description     varchar(1000) null comment '任务描述',
+    error_msg       text          null comment '异常信息',
+    begin_date_time datetime      not null comment '任务开始时间',
+    end_date_time   datetime      null comment '任务结束时间',
+    create_by       bigint        not null comment '创建人',
+    create_time     datetime      not null comment '创建时间',
+    update_by       bigint        not null comment '更新人',
+    update_time     datetime      not null comment '更新时间'
+)comment '任务信息表';
+
+
+
+
+
+
 
